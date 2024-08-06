10aad3730f905de142a3b13bb05898dcb2d946c7
mahaputrailhamawal <mahaputrailhamawal@gmail.com>
2024-08-06 21:34:32 +0700

    add: src/libs/fetch.ts dan src/types/entity.ts

---
+++ src/libs/fetch.ts
@@ -0,0 +1,14 @@
+export async function fetchData<T>(url:string, tag?:string) {
+    try{
+        const res = await fetch(url);
+
+        if (res.status !== 200){
+            throw new Error(`Error fetching data with tag ${tag}`);
+        }
+
+        const data = (await res.json()) as T;
+        return data;
+    } catch (error){
+        console.log(error);
+    }
+}
